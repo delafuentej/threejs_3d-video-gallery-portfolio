b49b805bc37cbe30392d416c236a3cfc53744e14
Jesús de la Fuente <jfuentelucas@gmail.com>
2025-11-06 12:13:55 +0100

    public files cleaned

--- src/hooks/useThreeScene.js
+++ src/hooks/useThreeScene.js
@@ -101,7 +101,7 @@ export default function useThreeScene(videos, options = {}) {
           scale: [20, 20, 20],
           materialOptions: {
             transparent: true,
-            opacity: 0.2,
+            opacity: 0.3,
             depthWrite: false,
           },
         });
